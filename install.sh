6f97839a50088ec867ae866f664402dd4e83cd97
Nicolas HENRI <nicolas.henri@radiofrance.com>
2023-09-16 21:33:20 +0200

    Setup repo

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+mkdir -p ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
+
+cp {CompareTools.plist,vscode.sh} ~/Library/Application\ Support/com.fournova.Tower3/CompareTools/
+
+echo "Install completed! 🚀"
+echo "Restart Git Tower and go to settings > Git Config and select Visual Studio Code in Diff tool and Merge tool"
